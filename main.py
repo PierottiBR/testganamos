2bc4bbec032d9a5be9599d87dbf8bc3d86594162
Braulio Pierotti <148255441+PierottiBR@users.noreply.github.com>
2025-04-01 12:27:33 -0300

    new page

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 from datetime import datetime
 import re
+from streamlit_card import card
 
 # Configuración
  # Cambiar por tu URL real
@@ -10,3 +11,24 @@ st.set_page_config(
     page_icon="💳",
     layout="wide"
 )
+
+
+
+def show_premios():
+    card(
+        title="Campeon 🏆",
+        text="Equipo Campeon",
+        image="https://placekitten.com/500/500",
+        styles={
+            "card": {
+                "width": "100%",
+                "height": "200px",
+                "border-radius": "60px",
+                "box-shadow": "0 0 50px rgba(255, 215, 0, 1)",
+                "margin-bottom": "0px"
+            },
+            "filter": {
+                "background-color": "rgba(0, 0, 0, 0)"  # <- make the image not dimmed anymore
+            }
+        }
+    )
